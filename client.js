61d1eb4927e5d13483db042ba6a66d50ebc8802a
Samicap <lovestochasewindmills@gmail.com>
2020-12-06 20:26:26 +0000

    added code

--- client.js
+++ client.js
@@ -1,20 +1,43 @@
 const net = require('net');
 
+let directions = {
+  0: "up",
+  1: "down",
+  2: "left",
+  3: "right"
+}
+
+function getMove(){
+  let n = Math.floor(Math.random() * 3)
+  return "Move: " + directions[n]
+}
+
+function submitMove(conn){
+  let move = getMove()
+  console.log(move)
+  conn.write(move)
+}
+
+
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '135.23.222.131',
-    port: 50542
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
+    host: 'localhost',
+    port: 50541,
 
-  conn.on("data", (data) => {
-    console.log(data)
   });
+
+  conn.setEncoding('utf8');
+
+  // conn.listen(port, function() {
+  //   console.log('Server Running');
+  // }),
+
 
   conn.on("connect", () => {
     console.log("successfuly connected")
-    conn.write("Name: SAM")
+    conn.write('Name: SAM');
+
+    setTimeout(submitMove, 50, conn)
   });
 
   return conn;
--- play.js
+++ play.js
@@ -1,31 +1,6 @@
 const net = require('net');
 const connect = require('./client');
+
 console.log('Connecting ...');
 connect();
-// /**
-//  * Establishes connection with the game server
-//  */
-// const connect = function() {
-//   const conn = net.createConnection({ 
-//     host: '135.23.222.131',
-//     port: 50542
-//   });
-//   // interpret incoming data as text
-//   conn.setEncoding('utf8'); 
-
-//   conn.on("data", (data) => {
-//     console.log(data)
-//   });
-
-//   conn.on("connect", () => {
-//     console.log("successfuly connected")
-//     conn.write("Name: SAM")
-//   });
-
-//   return conn;
-// }
-
-// console.log('Connecting ...');
-// connect();
-
 
